fb626763c766a725422b8c3a5dda66bd7a4b0638
Iankat <ianka.ferrada@gmail.com>
2022-04-18 08:35:06 -0400

    HTML nuevas pantallas + CSS

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@
 // Para incluir los diferentes sets de cartas podemos _importar_ el archivo
 // JavasSript que contenga el `export` correspondiente...
 //
-// import pokemon from '../data/pokemon/pokemon.js';
-// console.log(pokemon);
+//import 'Frontend Web Dev' from '../data/webdev/webdev.js';
+//console.log(webdev);
 //
 // O alternativamente podríamos cargar el JSON de forma asíncrona usando
 // `fetch` en el momento que consideremos necesario.
@@ -14,13 +14,43 @@
 //   .catch(console.error);
 //
 
-const App = () => {
+import colors from '../data/colors/colors.js';
+console.log(colors)
+
+let data = colors;
+import colors from '../data/colors/colors.js';
+console.log(colors)
+
+const App = () =>{
   const el = document.createElement('div');
 
   el.className = 'App';
   el.textContent = 'Hola mundo!';
 
   return el;
+};
+
+const divColor = () => {
+  let colorsArray = [];
+  console.log(colorsArray);
+
+
+  for (let i = 0; i < 8; i++) {
+    const bgColor = document.createElement("div");
+    const image = document.createElement("img");
+    bgColor.className = "card";
+    image.className = "image";
+    image.setAttribute("src", colors.items[i].image);
+    card.appendChild(image);
+
+    colorsArray.push(card);
+  }
+
+  return colorsArray;
+};
+
+
+ 
 };
 
 export default App;
--- src/main.js
+++ src/main.js
@@ -1,3 +1,26 @@
 import App from './components/App.js';
+//import colors from './data/colors/colors.js';
 
+document.getElementById("play").onclick = goToMemory;
+function goToMemory() {
+   location.href = "start.html";
+   let name = document.getElementById("name").value;
+   localStorage.setItem("name", name);
+};
+
+//function gettingUserName() {
+  // console.log(localStorage.getElementById("name").value);
+  // document.getElementById("greeting").value = "¡Hola" + localStorage.getItem("name") + "!";
+//}
+//gettingUserName();
+
+"memoryanimals.html"
+
+const colorsArray = App();
 document.getElementById('root').appendChild(App());
+
+for (let i=0; i < colorsArray.length; i++) {
+document.getElementById('cardcolors').appendChild(colorsArray [i]);
+
+console.log(colorsArray)
+}
