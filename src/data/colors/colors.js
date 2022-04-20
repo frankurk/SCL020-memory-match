f9940ee2c9a7fee32f0e315f2b7f0186ae19edce
Francisca Caro <francisca@caro.cl>
2022-04-20 08:16:19 -0400

    random function

--- src/data/colors/colors.js
+++ src/data/colors/colors.js
@@ -3,42 +3,42 @@ export default {
   items: [
     {
       id: 'Navy',
-      image: '../src/assets/img/Navy.jpg',
+      image: 'assets/img/Navy.jpg',
       bgColor: '#339933'
     },
     {
       id: 'Navy',
-      image: '../src/assets/img/Navy1.jpg',
+      image: 'assets/img/Navy1.jpg',
       bgColor: '#339933'
     },
     {
       id: 'Turquoise',
-      image: '../src/assets/img/Turquoise.png',
+      image: 'assets/img/Turquoise.png',
       bgColor: '#339933'
     },
     {
       id: 'Turquoise',
-      image: '../src/assets/img/Turquoise1.png',
+      image: 'assets/img/Turquoise1.png',
       bgColor: '#F05032'
     },
     {
       id: 'Magenta',
-      image: '../src/assets/img/Magenta.png',
+      image: 'assets/img/Magenta.png',
       bgColor: '#F05032'
     },
     {
       id: 'Magenta',
-      image: '../src/assets/img/Magenta1.png',
+      image: 'assets/img/Magenta1.png',
       bgColor: '#F05032'
     },
     {
       id: 'Peach',
-      image: '../src/assets/img/Peach.png',
+      image: 'assets/img/Peach.png',
       bgColor: '#1572B6'
     },
     {
       id: 'Peach',
-      image: '../src/assets/img/Peach1.png',
+      image: 'assets/img/Peach1.png',
       bgColor: '#1572B6'
     },
   ],
